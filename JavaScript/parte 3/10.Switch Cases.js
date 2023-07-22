1f538e996395e5c2df56a5acaaa6a1f8719fdf5c
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-22 13:20:32 -0300

    fim parte 3

---
+++ JavaScript/parte 3/10.Switch Cases.js
@@ -0,0 +1,23 @@
+/*
+    Switch Case - Controlador de Fluxo
+*/
+
+const temperature = 10
+
+switch (temperature) {
+    case 10:
+        console.log('Ta frio!')
+        break;
+
+    case 20:
+        console.log('Ta meio quente!')
+        break;
+
+    case 30:
+        console.log('Ta quente!')
+        break;
+
+    default:
+        console.log('Não sei dizer.')
+        break;
+}
