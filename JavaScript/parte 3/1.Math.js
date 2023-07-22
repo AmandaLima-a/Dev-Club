bda6b42a4cafc1d48ddd14ee800902a46ece55a1
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-21 23:22:58 -0300

    add projeto radom

---
+++ JavaScript/parte 3/1.Math.js
@@ -0,0 +1,28 @@
+/* 
+    Math -> Matemática
+        - pow (2,2) / potencia 
+        - sqrt (25,2) / raiz 
+        -PI
+        -ceil / teto -> arredondar para cima
+        floor / chão -> arredondar para baixo
+
+        -random() / número aleatório entre 0 e 1
+*/
+
+/* POW:
+    const result = Math.pow(2,2)
+    console.log(result)
+
+   SQRT:
+    const result = Math.sqrt(25,2)
+    console.log(result)
+
+   CEIL:
+    const result = Math.ceil(3.14)
+    console.log(result)
+
+*/
+
+const result = Math.random()
+console.log(result)
+
