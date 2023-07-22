a0ef00a3b140208808d67d1b9fe621f7a31ec197
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-22 00:20:06 -0300

    type of

---
+++ JavaScript/parte 3/7.Operador-typeof-delet.js
@@ -0,0 +1,20 @@
+/*
+    Operadores Type of e delete:
+        tipeof -> tipo do dado
+        delete -> deletar
+*/
+
+const myobject = {
+    name: 'Amanda',
+    age:20,
+    address: 'Rua dos Bobos, n0'
+}
+
+console.log(typeof myobject)
+
+
+const myNumber = '30'
+
+console.log(typeof myNumber)
+
+// delete myobject.address 
