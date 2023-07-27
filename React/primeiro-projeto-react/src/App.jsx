f4f676f49f13878f797d2c506508191fc3ef71f8
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-27 16:03:54 -0300

    atualizando primeiro projeto react

--- React/primeiro-projeto-react/src/App.jsx
+++ React/primeiro-projeto-react/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
 
 function App() {
+  // Código JavaScript:
 
-  return (
-      <div>
-        Hello React
-      </div>
+  const [list, setList] = useState([{ id: uuid(), task: "Nada" }])
+
+
+  function inputMudou(event) {
+    console.log(event.target.value)
+    setList([{ id: uuid(), task: event.target.value }])   // Adiciona um item dentro do array
+
+    console.log(list)
+  }
+
+  function cliqueiNoBotao() {
+    console.log('Cliquei')
+  }
+
+  // Retorna código HTML
+  return ( // Para usar javaScript aqui dentro tem q ter "{}"
+    <div>
+      <input onChange={inputMudou} placeholder="O que tenho que fazer..." />
+      <button onClick={cliqueiNoBotao}>Adicionar</button>
+
+      <ul>
+        {
+          list.map(item => (
+            <li key={item.id}>{item.task}</li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
